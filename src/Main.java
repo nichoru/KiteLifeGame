237ff993c0fab2103a3a4d3884a462fb994ebd8f
nichoru <nichoru@whs.school.nz>
2024-05-10 10:28:35 +1200

    Started working on the window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+import java.awt.event.*;
+import java.awt.geom.*;
 
+public class Main extends JFrame implements ActionListener, MouseListener {
+    public static void main(String[] args) {
+        
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void mouseExited(MouseEvent e) {System.out.println("exit");}
+    public void mouseEntered(MouseEvent e) {System.out.println("enter");}
+    public void mouseReleased(MouseEvent e) {System.out.println("release");}
+    public void mousePressed(MouseEvent e) {System.out.println("press");}
+    public void mouseClicked(MouseEvent e) {
+        int mouseX = e.getX();
+        int mouseY = e.getY();
+        System.out.println("click at "+mouseX+", "+mouseY);
+        JDialog box = new JDialog(this);
+        box.setBounds(400,400,170,20);
+        box.toFront();
+        box.setVisible(true);
+        box.setTitle("something witty");
     }
 }
